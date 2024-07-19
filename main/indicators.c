e35e0d10b0b2a3543b95192b3206a48aacfe9821
valivia <aniviavalor@gmail.com>
2024-07-19 18:00:51 +0100

    🐛 Fixed missing intr type on indicator

--- main/indicators.c
+++ main/indicators.c
@@ -6,6 +6,7 @@ void indicators_configure_gpio()
 {
     gpio_config_t io_conf;
     io_conf.pin_bit_mask = ((1ULL << INDICATOR_1_PIN) | (1ULL << INDICATOR_2_PIN));
+    io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
